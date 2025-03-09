043a230de2226b292f1e37b2e2fe063dfcecc6f6
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2025-03-09 23:56:00 +0900

    빌드 설정 변경: CSS 처리를 위한 postcss 플러그인 추가

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
+import image from "@rollup/plugin-image";
+import copy from "rollup-plugin-copy";
+import postcss from "rollup-plugin-postcss";
 import pkg from "./package.json";
 
 export default {
@@ -20,11 +23,19 @@ export default {
 		},
 	],
 	plugins: [
-		peerDepsExternal(),
+		peerDepsExternal({
+			includeDependencies: true,
+		}),
 		resolve({
 			extensions: [".js", ".jsx", ".ts", ".tsx"],
 		}),
 		commonjs(),
+		postcss({
+			extensions: ['.css'],
+			minimize: true,
+			inject: false,
+			extract: false
+		}),
 		typescript({
 			tsconfig: "./tsconfig.json",
 			exclude: ["**/__tests__/**", "**/*.test.ts", "**/*.test.tsx"],
@@ -35,6 +46,18 @@ export default {
 			},
 		}),
 		terser(),
+		image(),
+		copy({
+			targets: [
+				{ src: "src/assets/icons/*", dest: "dist/assets/icons" },
+				{ src: "src/fonts.css", dest: "dist" },
+				{ src: "src/components/**/*.ts", dest: "dist/src/components" },
+				{ src: "src/components/**/*.tsx", dest: "dist/src/components" },
+				{ src: "src/utils/**/*.ts", dest: "dist/src/utils" },
+				{ src: "src/plugins/**/*.ts", dest: "dist/src/plugins" },
+				{ src: "src/lib/**/*.ts", dest: "dist/src/lib" },
+			],
+		}),
 	],
-	external: ["react", "react-dom", "@emotion/styled", "slate", "slate-react"],
+	external: ["react", "react-dom", "@emotion/styled"],
 };
