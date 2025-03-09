18d262cf1146c45e8f988381d16ff51db26c7ff9
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2025-03-09 18:49:12 +0900

    first publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,40 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.tsx',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.lib.json',
-      useTsconfigDeclarationDir: true,
-    }),
-  ],
-  external: ['react', 'react-dom', '@emotion/styled', 'slate', 'slate-react', 'slate-history'],
-}; 
+	input: "src/lib/index.ts",
+	output: [
+		{
+			file: "dist/index.js",
+			format: "cjs",
+			sourcemap: true,
+		},
+		{
+			file: "dist/index.esm.js",
+			format: "esm",
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		peerDepsExternal(),
+		resolve({
+			extensions: [".js", ".jsx", ".ts", ".tsx"],
+		}),
+		commonjs(),
+		typescript({
+			tsconfig: "./tsconfig.json",
+			exclude: ["**/__tests__/**", "**/*.test.ts", "**/*.test.tsx"],
+			compilerOptions: {
+				sourceMap: true,
+				declaration: true,
+				declarationDir: "dist",
+			},
+		}),
+		terser(),
+	],
+	external: ["react", "react-dom", "@emotion/styled", "slate", "slate-react"],
+};
